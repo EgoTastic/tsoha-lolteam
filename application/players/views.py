56266b90d0054206f323358170c3dc9935f52ae6
Miika <miika.f.koskela@gmail.com>
2020-03-19 00:44:03 +0200

    korjaus34

--- application/players/views.py
+++ application/players/views.py
@@ -13,39 +13,39 @@ def players_create():
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players", methods=["GET"])
+@app.route("/players/", methods=["GET"])
 def players_index():
     return render_template("players/list.html", players = Player.query.all())
 
-@app.route("/players/<player_id>", methods=["POST"])
+@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_top(player_id):
     pl = Player.query.get(player_id)
     pl.top = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>", methods=["POST"])
+@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_jgl(player_id):
     pl = Player.query.get(player_id)
     pl.jgl = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>", methods=["POST"])
+@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_mid(player_id):
     pl = Player.query.get(player_id)
     pl.mid = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>", methods=["POST"])
+@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_adc(player_id):
     pl = Player.query.get(player_id)
     pl.adc = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>", methods=["POST"])
+@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_sup(player_id):
     pl = Player.query.get(player_id)
     pl.sup = True
