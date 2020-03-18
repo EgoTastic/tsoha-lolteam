cb0224b044ba62b7bd3bc2e6a6a598bb21996ecb
Miika <miika.f.koskela@gmail.com>
2020-03-19 00:47:23 +0200

    korjattu views.py app routet

--- application/players/views.py
+++ application/players/views.py
@@ -24,28 +24,24 @@ def players_set_top(player_id):
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_jgl(player_id):
     pl = Player.query.get(player_id)
     pl.jgl = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_mid(player_id):
     pl = Player.query.get(player_id)
     pl.mid = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_adc(player_id):
     pl = Player.query.get(player_id)
     pl.adc = True
     db.session().commit()
     return redirect(url_for("players_index"))
 
-@app.route("/players/<player_id>/", methods=["POST"])
 def players_set_sup(player_id):
     pl = Player.query.get(player_id)
     pl.sup = True
